3398268e6daf95393b40b42ba4def6c0e21d6243
DominikStyp <dominik.stypula1@gmail.com>
2020-01-01 23:44:04 +0100

    Materialize 4.8.2 installation + added 3 sample components, and Header basing on materialize components

--- material-ui-project1/src/App.js
+++ material-ui-project1/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./Layouts/Header";
+import Body from "./Layouts/Body";
+import Footer from "./Layouts/Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Body />
+    <Footer />
     </div>
   );
 }
---
+++ material-ui-project1/src/Layouts/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Body extends React.Component
+{
+    render() {
+        return (
+            <div>
+                Body
+            </div>
+        );
+    }
+}
+
+export default Body;
