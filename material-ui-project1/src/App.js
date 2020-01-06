6724b86dceea3849470393fb8124cb2954d9216b
DominikStyp <dominik.stypula1@gmail.com>
2020-01-06 22:38:08 +0100

    use grid element

--- material-ui-project1/src/App.js
+++ material-ui-project1/src/App.js
@@ -3,11 +3,13 @@ import PrimarySearchAppBar from "./Layouts/PrimarySearchAppBar";
 import Body from "./Layouts/Body";
 import Footer from "./Layouts/Footer";
 import './App.css';
+import MyGridElement from "./Layouts/MyGridElement";
 
 function App() {
   return (
     <div className="App">
     <PrimarySearchAppBar />
+    <MyGridElement/>
     <Body />
     <Footer />
     </div>
